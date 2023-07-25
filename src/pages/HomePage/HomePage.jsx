6e3061a197df7938ea423acf69e10bfc7370d72f
kharisma2806 <kiran.nankani28@gmail.com>
2023-07-25 20:21:51 +0200

    petprofile done

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,46 +2,65 @@ import React, { useEffect, useState } from "react";
 import { useContext } from "react";
 import { AuthContext } from "../../context/auth.context";
 import PetsPage from "../PetsPage/PetsPage";
-import LoginPage from "../LoginPage/LoginPage";
+import LandingPage from "../LandingPage/LandingPage"
 import AliceCarousel from "react-alice-carousel";
+import { Image } from "cloudinary-react";
+import axios from "axios";
 import "react-alice-carousel/lib/scss/alice-carousel.scss";
 import "./HomePage.css";
 
 function HomePage() {
   const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
   const [pets, setPets] = useState([]);
+  const [publicId, setPublicId] = useState("");
 
   useEffect(() => {
-    if (user && user.preference) {
-      if (user.preference === "Dogs") {
-        getDogs();
-      } else if (user.preference === "Cats") {
-        getCats();
-      } else if (user.preference === "Small Pets") {
-        getSmallPets();
-      } else {
-        getAllPets();
+    const fetchData = async () => {
+      if (user && user.preference) {
+        let apiUrl = "";
+  
+        if (user.preference === "Dogs") {
+          apiUrl = "http://localhost:5005/pet/dogs";
+        } else if (user.preference === "Cats") {
+          apiUrl = "http://localhost:5005/pet/cats";
+        } else if (user.preference === "Small Pets") {
+          apiUrl = "http://localhost:5005/pet/small_animals";
+        } else {
+          apiUrl = "http://localhost:5005/pet/pets";
+        }
+  
+        const res = await fetch(apiUrl);
+        const finalRes = await res.json();
+        setPets(finalRes);
+  
+        // Get the public ID of the first pet in the fetched data
+        if (finalRes.length > 0) {
+          setPublicId(finalRes[0].image);
+        }
       }
-    }
+    };
+  
+    fetchData();
   }, [user]);
+  
 
   const getDogs = async () => {
-    const res = await fetch("http://localhost:5005/api/dogs");
+    const res = await fetch("http://localhost:5005/pet/dogs");
     const finalRes = await res.json();
     setPets(finalRes);
   };
   const getCats = async () => {
-    const res = await fetch("http://localhost:5005/api/cats");
+    const res = await fetch("http://localhost:5005/pet/cats");
     const finalRes = await res.json();
     setPets(finalRes);
   };
   const getSmallPets = async () => {
-    const res = await fetch("http://localhost:5005/api/small_pets");
+    const res = await fetch("http://localhost:5005/pet/small_animals");
     const finalRes = await res.json();
     setPets(finalRes);
   };
   const getAllPets = async () => {
-    const res = await fetch("http://localhost:5005/api/all_pets");
+    const res = await fetch("http://localhost:5005/pet/pets");
     const finalRes = await res.json();
     setPets(finalRes);
   };
@@ -51,7 +70,11 @@ function HomePage() {
       <div key={pet.id} className="carousel-slide">
         <div className="pet-card">
           <div className="pet-image">
-            <img src={pet.image} alt={pet.name} />
+            <Image
+              cloudName="dnstseshn"
+              publicId={pet.image} // Use the correct property here
+            />
+            {/* <img src={pet.image} alt={pet.name} /> */}
           </div>
         </div>
         <div className="pet-details">
@@ -67,6 +90,7 @@ function HomePage() {
       </div>
     ));
   };
+  
   
   
 
@@ -102,7 +126,7 @@ function HomePage() {
             <p>No pets available.</p>
           )
         ) : (
-          <LoginPage />
+          <LandingPage />
         )}
       </div>
     </div>
