e3a21c1b948a31e5be41adfc1924085bbafb7416
Barbara Ramirez <brcmua@gmail.com>
2023-07-26 00:30:56 +0200

    owner profile

--- src/pages/OwnerProfilePage/OwnerProfilePage.jsx
+++ src/pages/OwnerProfilePage/OwnerProfilePage.jsx
@@ -1,10 +1,29 @@
 // OwnerProfilePage.jsx
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { useContext } from "react"
+import { AuthContext } from "../../context/auth.context";
+import catIcon from "../../images/cat-profile.jpg";
+import bgImg from "../../images/bg-landing-2.jpg";
+
+
+import io from "socket.io-client";
+const socket = io.connect("http://localhost:5005");
 
 function OwnerProfilePage() {
+  const { user} = useContext(AuthContext);
   const { ownerId } = useParams();
+  const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
+  
+  const [username, setUsername] = useState("");
   const [owner, setOwner] = useState(null);
+
+
+  const joinRoom = () => {
+    if (username !== "" && room !== "") socket.emit("join_room", room);
+    setShowChat(true);
+  };
 
   useEffect(() => {
     const fetchOwnerData = async () => {
@@ -24,12 +43,31 @@ function OwnerProfilePage() {
   }
 
   return (
-    <div>
-      <h1>Owner Profile</h1>
-      <p>Name: {owner.ownerName}</p>
-      <p>Email: {owner.ownerEmail}</p>
-      <p>Phone: {owner.ownerPhone}</p>
-      {/* Add other owner details you want to display */}
+    <div style={{backgroundImage: `url(${bgImg})`, objectFit: "scale-down" }}>
+
+<div className="flex justify-center py-12 px-6">
+      <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <div className="flex flex-col items-center pb-10">
+          <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src={catIcon} alt="Profile picture"/>
+          <h5 className="mb-1 text-2xl font-medium text-gray-900 dark:text-white">{owner.ownerName}</h5>
+          <span className="text-sm text-gray-500 dark:text-gray-400 py-3">{owner.zip} , {owner.city} , {owner.country}</span>
+          <span className="text-sm text-gray-500 dark:text-gray-400 py-3"><span className="font-bold">Email:</span> {owner.ownerEmail}</span>
+          <span className="text-sm text-gray-500 dark:text-gray-400 py-3">Phone: <span className="font-bold">{owner.ownerPhone}</span></span>
+
+      </div>
+      
+      <div className="">
+                <h3 className="joinChatHeader">Join a chat</h3>
+                <input type="text" value={user.name} onChange={(event) => {setUsername(event.target.value);}} className="joinChatInput"/>
+                <input type="text" value={owner._id} onChange={(event) => {setRoom(event.target.value)}} className="joinChatInput" />
+                <button onClick={joinRoom} className="joinChatButton">
+                  Join a room
+                </button>
+      </div>
+  </div>
+      </div>
+
+
     </div>
   );
 }
